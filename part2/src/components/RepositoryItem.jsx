7dd6b38fd90debce61ca55bdf83cf5b4a22566a1
Andrés Correa <andrees92@hotmail.com>
2022-11-23 13:01:30 -0500

    finishing item details

--- part2/src/components/RepositoryItem.jsx
+++ part2/src/components/RepositoryItem.jsx
@@ -1,5 +1,5 @@
-import { View, StyleSheet, Image, Text } from 'react-native'
-//import Text from './Text'
+import { View, StyleSheet, Image } from 'react-native'
+import Text from './Text'
 import theme from '../utils/theme'
 const styles = StyleSheet.create({
 	card: {
@@ -8,27 +8,32 @@ const styles = StyleSheet.create({
 	},
 	card__header: {
 		display: 'flex',
-		flexDirection: 'row',
-    justifyContent: 'space-evenly',
+		flexDirection: 'row',    
     alignItems: 'flex-start',
-    padding: 5,
+    padding: 5, 
     marginBottom: 8
     
 	},
-	card__content: {
-		display: 'flex',
-    flexGrow:2,
-    alignItems: 'flex-start',
-    
-	},
-	card__footer: {
-		display: 'flex',
-		flexDirection: 'row',
-	},card__img:{
+  card_imgContainer:{
+    maxWidth: 60,
+    maxHeight: 60,    
+  },card__img:{
     width: 50,
     height: 50,
     borderRadius:5,
-    marginRight:10
+    marginRight:10,    
+  },
+	card__content: {     
+    flexGrow: 1,
+    flexShrink: 1,
+    padding:5,
+    alignItems: 'flex-start',
+	},
+  card__title:{    
+    marginBottom: 8,
+  },
+  card__subHeading:{
+    marginBottom: 8,       
   },
   card__btn:{
     backgroundColor: theme.colors.btn,
@@ -37,32 +42,63 @@ const styles = StyleSheet.create({
     color: theme.colors.white,
     textAlign:'center',
     paddingVertical: 6,
-    paddingHorizontal: 12,    
+    paddingHorizontal: 12,  
+    overflow: "hidden"  
   },
-  marginBottom:{
-    marginBottom: 8,
+	card__footer: {
+		display: 'flex',
+		flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+	},
+  card__detail:{
+    justifyContent: 'space-around',
+    alignItems: 'center'
     
   }
+
 })
+
+const valueDetails = (value) => {
+  
+  if(value <= 1000) return value
+  let valueCalculated = value/1000
+  const valueRounded = Math.round(valueCalculated*100)/100
+  return `${valueRounded.toFixed(1)}K` 
+  
+}
+
+const RepositoyDetail = ({detail, value}) => {
+
+  return (
+    <View style={styles.card__detail}>
+      <Text theming='primary' style={styles.card__title} > {value}</Text>
+      <Text theming='secondary' style={styles.card__subHeading}> {detail} </Text>
+    </View>
+  )
+}
 
 const RepositoryItem = ({ repository }) => {
   console.log(repository.ownerAvatarUrl)
 	return (
 		<View style={styles.card}>
 			<View style={styles.card__header}>
-				<Image style={styles.card__img} source={{uri: repository.ownerAvatarUrl}}/> 
+        <View style={styles.card_imgContainer}>
+          <Image style={styles.card__img} source={{uri: repository.ownerAvatarUrl}}/>
+        </View> 
 				<View style={styles.card__content}>
-					<Text theming='primary' style={styles.marginBottom}>{repository.fullName}</Text>
-					<Text style={styles.marginBottom}>{repository.description}</Text>
+					<Text theming='primary' style={styles.card__title}>{repository.fullName}</Text>
+					<Text theming='secondary' style={styles.card__subHeading}>{repository.description}</Text>
 					<Text style={styles.card__btn}>{repository.language}</Text>
 				</View>
 			</View>
-			<View style={styles.card__footer}>
-				<Text>Stars: {repository.stargazersCount}</Text>
-				<Text>Forks: {repository.forksCount}</Text>
-				<Text>Reviews: {repository.reviewCount}</Text>
-				<Text>Rating: {repository.ratingAverage}</Text>
+			<View style={styles.card__footer}>        
+          <RepositoyDetail detail='Starts' value={valueDetails(repository.stargazersCount)} />
+          <RepositoyDetail detail='Forks' value={valueDetails(repository.forksCount)} />
+          <RepositoyDetail detail='Reviews' value={valueDetails(repository.reviewCount)} />
+          <RepositoyDetail detail='Rating' value={valueDetails(repository.ratingAverage)} />     			
 			</View>
+
 		</View>
 	)
 }
