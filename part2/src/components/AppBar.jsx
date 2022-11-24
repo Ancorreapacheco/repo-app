42feeef5250248bce7fcef885c3b3050bc4b14f6
Andrés Correa <andrees92@hotmail.com>
2022-11-24 01:07:14 -0500

    Link ready

--- part2/src/components/AppBar.jsx
+++ part2/src/components/AppBar.jsx
@@ -8,29 +8,28 @@ import { Link } from 'react-router-native'
 const styles = StyleSheet.create({
 	container: {
 		paddingTop: Constants.statusBarHeight,
-		backgroundColor: theme.colors.secundary,		
-    flexDirection: 'row'
+		backgroundColor: theme.colors.secundary,
+		flexDirection: 'row',
+    justifyContent: 'space-evenly'
+		
 	},
-	
 })
 
 const AppBar = () => {
-	return ( 
+	return (
 		<View style={styles.container}>
-      <Text theming='tab'>Repositories</Text>
-      <Link> Some Route</Link>
+			<Link to='/'>
+				<Text theming='tab'>Repositories</Text>
+			</Link>
+			<Link to='/signin'>
+				<Text theming='tab'>Sign In</Text>
+			</Link>
 		</View>
-    //TODO fixing link and router exercise
-
-
-    
-    
-      
+		//TODO fixing link and router exercise
 	)
 }
 
 export default AppBar
 
-
-//TODO Another good idea might be to separate the app bar's tab into its own component such as 
+//TODO Another good idea might be to separate the app bar's tab into its own component such as
 //AppBarTab so that it is easy to add new tabs in the future.
